209e163d597325a46d3ab883f0b2383dbf931dce
Mathieu Berte <mathieu.berte@gmail.com>
2017-08-11 12:15:15 +0200

    Desktop version done

--- js/main.js
+++ js/main.js
@@ -264,10 +264,11 @@ $(window).on('scroll', function(){
         }
 
     function initMap() {
+        var Center = {lat: 45.7649915, lng: 4.8850808};
         var map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 45.7649915, lng: 4.8850808},
+          center: Center,
           zoom: 13,
-          styles: [
+            styles: [
                     {
                         "elementType": "geometry",
                         "stylers": [
@@ -426,8 +427,89 @@ $(window).on('scroll', function(){
                         }
                         ]
                     }
-                    ] });
-        }
+                    ] 
+        });
+        var contentString = '<div id="content">'+
+            '<div id="siteNotice">'+
+            '</div>'+
+            '<h2 class="Text-cyan">Mathieu Berte</h2>'+
+            '<div class="Map-data-cont">'+
+                '<img class="Map-data-img" src="images/location.svg"/>'+
+                '<p class="Text-white Map-data-txt">47 Rue Docteur Ollier<br>'+
+                '69100 Villeurbanne<br>'+
+                'France</p>'+
+            '</div>'+
+            '<div class="Map-data-cont">'+
+                '<img class="Map-data-img" src="images/phone.svg"/>'+
+                '<p class="Text-white Map-data-txt">+33 (0)6 87 19 36 96</p>'+
+            '</div>'+
+            '<div class="Map-data-cont">'+
+                '<img class="Map-data-img" src="images/email.svg"/>'+
+                '<p class="Text-white Map-data-txt">mathieu.berte@gmail.com</p>'+
+                '<div class="social">'+
+                    '<img class="Social-Btn" src="images/linkedin-btn.svg"/>'+
+                    '<img class="Social-Btn" src="images/twitter-btn.svg">'+         
+                '</div>'+
+            '</div>'+
+            '</div>';
+
+        var infowindow = new google.maps.InfoWindow({
+          content: contentString
+        });
+
+        var marker = new google.maps.Marker({
+          position: Center,
+          map: map,
+          title: 'Uluru (Ayers Rock)'
+          
+        });
+        marker.addListener('click', function() {
+          infowindow.open(map, marker);
+        });
+        infowindow.open(map, marker);
+        google.maps.event.addListener(infowindow, 'domready', function() {
+
+            // Reference to the DIV which receives the contents of the infowindow using jQuery
+            var iwOuter = $('.gm-style-iw');
+
+            /* The DIV we want to change is above the .gm-style-iw DIV.
+                * So, we use jQuery and create a iwBackground variable,
+                * and took advantage of the existing reference to .gm-style-iw for the previous DIV with .prev().
+                */
+            var iwBackground = iwOuter.prev();
+
+            // Remove the background shadow DIV
+            iwBackground.children(':nth-child(2)').css({'display' : 'none'});
+
+            // Remove the white background DIV
+            iwBackground.children(':nth-child(4)').css({'display' : 'none'});
+
+
+            // Moves the shadow of the arrow 76px to the left margin 
+            iwBackground.children(':nth-child(1)').attr('style', function(i,s){ return s + 'left: 76px !important;'});
+
+            // Moves the arrow 76px to the left margin 
+            iwBackground.children(':nth-child(3)').attr('style', function(i,s){ return s + 'left: 76px !important;'});
+            // Changes the desired color for the tail outline.
+            // The outline of the tail is composed of two descendants of div which contains the tail.
+            // The .find('div').children() method refers to all the div which are direct descendants of the previous div. 
+            iwBackground.children(':nth-child(3)').find('div').children().css({'box-shadow': 'rgba(72, 181, 233, 0.6) 0px 1px 6px', 'z-index' : '1', 'background-color' : '#282E41'});
+            
+            var iwCloseBtn = iwOuter.next();
+
+            // Apply the desired effect to the close button
+            iwCloseBtn.css({
+                opacity: '1', // by default the close button has an opacity of 0.7
+                right: '-35px', top: '23px', // button repositioning
+                });
+
+                // The API automatically applies 0.7 opacity to the button after the mouseout event.
+                // This function reverses this event to the desired value.
+                iwCloseBtn.mouseout(function(){
+                $(this).css({opacity: '1'});
+                });
+        });
+    }
     function GetProjects() {
         
         $.getJSON("https://mathieubertecreations.com/Site-Perso/js/Projects.json", function(data){
@@ -463,4 +545,3 @@ $(window).on('scroll', function(){
         var ProjectDetails = document.querySelector(".Project-details");
         ProjectDetails.classList.toggle("Project-details-closed");
     }
-    
